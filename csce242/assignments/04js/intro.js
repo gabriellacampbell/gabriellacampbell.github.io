d08b4cd4596e44e258e67944796833a7f768d5ba
Gabriella Campbell <gabygracecampbell@gmail.com>
2026-09-10 12:39:51 -0400

    add boucing ball

--- csce242/assignments/04js/intro.js
+++ csce242/assignments/04js/intro.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+//shows message when button is clicked
+document.getElementById("btn-show-message").onclick = (e) => {
+    document.getElementById("p-message").innerHTML = "hello world";
+    e.target.innerHTML = "done!";
+};
+
+//styles the link when it is clicked
+document.getElementById("link").onclick = (e) => {
+    e.target.classList.add("cool-link");
+    e.preventDefault(); //dont go to link destination
+};
+
+//animates the ball when it is clicked
+document.getElementById("btn-bounce").onclick = (e) => {
+    document.getElementById("ball").classList.add("bouncing-ball");
+};
